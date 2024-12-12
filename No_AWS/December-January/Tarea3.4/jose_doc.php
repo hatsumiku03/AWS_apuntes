816e6b00f65cef1f73cc594301e4aa95c7d7f499
Joselito <josalbarr@alu.edu.gva.es>
2024-12-12 10:02:29 +0100

    tarea 3.4 ended

---
+++ No_AWS/December-January/Tarea3.4/jose_doc.php
@@ -0,0 +1,16 @@
+
+/**
+ * nombresDeLosJinetesMalvados
+ *
+ * @param  mixed $nombre
+ * @param  mixed $alias
+ * @return void
+ */<?php
+function nombresDeLosJinetesMalvados($nombre, $alias) {
+    print "$nombre $alias | ";
+}
+
+nombresDeLosJinetesMalvados("Joselito", "Algoritmo Raro");
+nombresDeLosJinetesMalvados("Martilusk07", "Perro inteligente");
+nombresDeLosJinetesMalvados("Babu", "Dios apocaliptico, comedor de planetas");
+?>
