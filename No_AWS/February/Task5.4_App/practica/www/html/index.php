8590e45f560876d23fc86f610ded3d5f8b22dd46
Joselito <josalbarr@alu.edu.gva.es>
2025-02-12 10:16:30 +0100

    task 5.4 added

---
+++ No_AWS/February/Task5.4_App/practica/www/html/index.php
@@ -0,0 +1,31 @@
+     <!DOCTYPE html>
+     <head>
+      <title>¡Hola mundo!</title>
+     </head>
+
+     <body>
+      <h1>¡Hola mundo!</h1>
+      <p><?php echo 'Estamos corriendo PHP, version: ' . phpversion(); ?></p>
+      <?
+       $database ="mydb";
+       $user = "root";
+       $password = "secret";
+       $host = "mysql";
+
+       $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+       $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
+       $tables = $query->fetchAll(PDO::FETCH_COLUMN);
+
+        if (empty($tables)) {
+          echo "<p>No hay tablas en la base de datos \"{$database}\".</p>";
+        } else {
+          echo "<p>La base de datos \"{$database}\" tiene las siguientes tablas:</p>";
+          echo "<ul>";
+            foreach ($tables as $table) {
+              echo "<li>{$table}</li>";
+            }
+          echo "</ul>";
+        }
+        ?>
+    </body>
+</html>
